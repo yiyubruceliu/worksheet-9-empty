d28a607dd1dabe003a6e8bc3c9f3554b7ec6f76f
Liu Yi-Yu <yi-yu.liu@modul.ac.at>
2023-12-11 16:34:33 +0100

    a message describing the specific changes I have made this time

---
+++ main.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 11 16:27:47 2023
+
+@author: yliu
+"""
+
+import supportFile
+
+
+supportFile.saySomething()
+
+print('hello world')
---
+++ supportFile.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 11 16:29:08 2023
+
+@author: yliu
+"""
+
+def saySomething():
+    print('something')
